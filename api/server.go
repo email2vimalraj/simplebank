99ed1da697f2b94e791578a09318bbf91b0af826
Vimalraj Selvam <email2vimalraj@gmail.com>
2021-04-27 21:29:20 +0530

    created api server

---
+++ api/server.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	db "github.com/email2vimalraj/simplebank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Server servers HTTP requests for our banking service.
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and setup routing.
+func NewServer(store *db.Store) *Server {
+	server := &Server{
+		store: store,
+	}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	server.router = router
+	return server
+}
+
+// Start runs the HTTP server on a specific address.
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
