1f92156e22246910436082702481c02866dfae7b
Vimalraj Selvam <email2vimalraj@gmail.com>
2021-04-27 21:37:09 +0530

    added viper for reading config file

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,20 +6,21 @@ import (
 	"os"
 	"testing"
 
+	"github.com/email2vimalraj/simplebank/util"
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+
 	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
